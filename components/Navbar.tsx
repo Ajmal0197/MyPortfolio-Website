932802a443bce33345d13f74cfae5a19b86f7574
AjmalT <>
2024-09-13 22:14:59 +0530

    fixed lint

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,3 @@
-import logo from '../assets/kevinRushLogo.png';
-import Image from 'next/image';
 import { FaLinkedin } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
 import { FaDev } from 'react-icons/fa';
@@ -11,7 +9,7 @@ interface NavbarProps {
     // links: Array<{ name: string; url: string }>;
 }
 
-const Navbar: FC<NavbarProps> = (props) => {
+const Navbar: FC<NavbarProps> = () => {
     return (
         <nav className='fixed top-0 left-0 right-0 z-50 flex items-center justify-center py-6 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 shadow-md'>
             <div className="flex flex-shrink-0 items-center">
